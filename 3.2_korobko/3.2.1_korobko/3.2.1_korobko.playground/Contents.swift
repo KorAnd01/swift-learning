b76f673d348ea7adb4c78b0eeb4b79a48749d7c1
Andrey <dronisey@mail.ru>
2025-03-15 20:14:42 +0100

    3.2_korobko

---
+++ 3.2_korobko/3.2.1_korobko/3.2.1_korobko.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let userName: String = "Andrey Korobko"
+let userCitizenship: String = "Rus"
+var userAge: UInt = 38
+let userHeight: Double = 193.5
+var userVeight: Float = 90.3
+var userMarrid: Bool = true
+
+print ("имя:", userName)
+print ("гражданство:", userCitizenship)
+print ("возраст:", userAge)
+print ("рост:", userHeight)
+print ("вес:", userVeight)
+print ("семейное положение", userMarrid)
+
---
+++ 3.2_korobko/3.2.2_korobko/3.2.2_korobko.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let swiftLearning = 50
+var swiftLearning2 = swiftLearning
+
+swiftLearning == swiftLearning2
+
+swiftLearning2 = -swiftLearning2
+swiftLearning == swiftLearning2
