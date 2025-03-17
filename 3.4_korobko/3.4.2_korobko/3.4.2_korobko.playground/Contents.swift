c21629449b4d42afa7c1f6c1bcdafdb3d1966f08
Andrey <dronisey@mail.ru>
2025-03-17 22:01:16 +0100

    3.4.2_korobko

--- 3.4_korobko/3.4.2_korobko/3.4.2_korobko.playground/Contents.swift
+++ 3.4_korobko/3.4.2_korobko/3.4.2_korobko.playground/Contents.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 var string = "Сегодня был теплый, солнечный день"
+let string0 = "Сегодня был теплый, солнечный день"
+var stringFrozen = "морозный"
+var string1 = "Сегодня был морозный, солнечный день"
 
-//string.replace("теплый", with: "морозный")
+string.replace("теплый", with: "морозный")
 
 let leftIndex = string.index(string.startIndex, offsetBy: 12)
-let rightIndex = string.index(string.startIndex, offsetBy: 19)
+let rightIndex = string.index(string.startIndex, offsetBy: 17)
 string.replaceSubrange(leftIndex...rightIndex, with: "морозный")
 
-print(string)
+print("было:", string0)
+print("поменяли теплый на", stringFrozen)
+print("стало:", string1)
