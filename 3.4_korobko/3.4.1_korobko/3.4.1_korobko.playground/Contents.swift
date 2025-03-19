eab13aaa3161ad89462bd35407624da946635bdb
Andrey <dronisey@mail.ru>
2025-03-19 20:31:08 +0100

    3.4_korobko

--- 3.4_korobko/3.4.1_korobko/3.4.1_korobko.playground/Contents.swift
+++ 3.4_korobko/3.4.1_korobko/3.4.1_korobko.playground/Contents.swift
@@ -6,14 +6,9 @@ let myAge = "39 "
 let myHobby = "sport "
 let myDreamSalary = "3000"
 
-var resume = myName+myLastName+myAge+myHobby+myDreamSalary
+var resume = myNamу + myLastName + myAge + myHobby + myDreamSalary
 print(resume)
 
 var myFirstName = "A\nn\nd\nr\ne\ny\n"
 print(myFirstName)
 
-/* другой способ
- let FirstName = "Andrey"
-FirstName.forEach {char in
-    print(char)
-} */
