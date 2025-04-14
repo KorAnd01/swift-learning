9216dc63bb01f64379a5631db69290f5249d0276
Andrey <dronisey@mail.ru>
2025-04-14 22:02:25 +0200

    3.9_korobko

--- 3.9_korobko/3.9.1_korobko/3.9.1_korobko.playground/Contents.swift
+++ 3.9_korobko/3.9.1_korobko/3.9.1_korobko.playground/Contents.swift
@@ -3,8 +3,8 @@ import UIKit
 var dictionary = ["Гомер": "Douh", "Барт": "карамба"]
 dictionary["Фландерс"] = "дидли-дудли"
 
-for key in dictionary.keys {
-    print("key: \(key)")
+for (key, value) in dictionary {
+     print("key: \(key), value: \(value)")
 }
 
 
--- 3.9_korobko/3.9.2_korobko/3.9.2_korobko.playground/Contents.swift
+++ 3.9_korobko/3.9.2_korobko/3.9.2_korobko.playground/Contents.swift
@@ -7,38 +7,16 @@ let nikName = ["Мария": 90,
                    "Иван": 80,
                    "Сергей": 39]
 
+let keyses = Array(bonuses.keys) + Array(nikName.keys)
+
 var summ: [String: Int] = [:]
 
-/* по логике в переменную summ добавляю поочередно первый словарь по ключам,
- потом второй по ключам.
- Затем также c бонусами, но указать условие, что если значения по ключам совпадают, то бонусы суммируются */
-
-
-for Keybonuses in bonuses.keys {
-    if let a = bonuses[Keybonuses] {
-        for keyNikName in nikName.keys {
-            if let b = nikName[keyNikName] {
-                let summ1 = a + b
-                print(summ1)
-            }
-        }
-    }
-}
-
-for (valueBonus1, valueBonus) in bonuses {
-    if let c = bonuses[valueBonus] {
+for key in bonuses.keys {
+    
+    if let value1 = bonuses[key], let value2 = nikName[key] {
+            
+        summ[key] = value1 + value2
         
-        for (valueNikName1, valueNikName) in nikName {
-            if let d = nikName[valueNikName] {
-                
-            }
         }
-    }
 }
-
-if let Keybonuses = keyNikName
-    let summ2 = c + d
-
-summ = summ1 + summ2
-
 print(summ)
