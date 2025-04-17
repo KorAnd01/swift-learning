57bfcf5d79f6b7ee7d2a997f40937d138e771c3e
Andrey <dronisey@mail.ru>
2025-04-17 23:20:01 +0200

    3.9_korobko

--- 3.9_korobko/3.9.2_korobko/3.9.2_korobko.playground/Contents.swift
+++ 3.9_korobko/3.9.2_korobko/3.9.2_korobko.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-let bonuses = ["Андрей": 100,
+var bonuses = ["Андрей": 100,
                   "Владимир": 50,
                   "Сергей": 1]
 let nikName = ["Мария": 90,
@@ -9,14 +9,29 @@ let nikName = ["Мария": 90,
 
 let keyses = Array(bonuses.keys) + Array(nikName.keys)
 
-var summ: [String: Int] = [:]
-
-for key in bonuses.keys {
+var summ = [String: Int]()
     
-    if let value1 = bonuses[key], let value2 = nikName[key] {
-            
-        summ[key] = value1 + value2
-        
+for key in keyses {
+   
+    if let key1 = bonuses[key], let key2 = nikName[key] {
+       var summ = key1 + key2
+        print(summ)
+    } else {
+   if let key1 = bonuses[key], let key2 = nikName[key] {
+            key1 != key2
+            let  summ = key1
+            print(summ)
+    } else {
+            if let key2 = nikName[key], let key1 = bonuses[key]  {
+                key2 != key1
+                let  summ = key2
+                print(summ)
+           /* } else {
+                let summ = 0
+                print("нет значения") */
+            }
         }
+    }
 }
 print(summ)
+
