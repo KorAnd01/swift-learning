e1d97f3e0c3449a7f78a4b28043b15d98fb1bd5d
Andrey <dronisey@mail.ru>
2025-04-20 07:35:27 +0200

    3.9_korobko

--- 3.9_korobko/3.9.2_korobko/3.9.2_korobko.playground/Contents.swift
+++ 3.9_korobko/3.9.2_korobko/3.9.2_korobko.playground/Contents.swift
@@ -14,24 +14,16 @@ var summ = [String: Int]()
 for key in keyses {
    
     if let key1 = bonuses[key], let key2 = nikName[key] {
-       var summ = key1 + key2
+       summ[key] = key1 + key2
         print(summ)
+    } else if let key1 = bonuses[key] {
+            summ[key] = key1
+    } else if let key2 = nikName[key] {
+                summ[key] = key2
+        
     } else {
-   if let key1 = bonuses[key], let key2 = nikName[key] {
-            key1 != key2
-            let  summ = key1
-            print(summ)
-    } else {
-            if let key2 = nikName[key], let key1 = bonuses[key]  {
-                key2 != key1
-                let  summ = key2
-                print(summ)
-           /* } else {
-                let summ = 0
-                print("нет значения") */
+    print("нет значения")
             }
         }
-    }
-}
 print(summ)
 
