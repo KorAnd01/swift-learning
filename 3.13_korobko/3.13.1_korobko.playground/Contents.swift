cde08ae001cb324aa0053eeec25824486cd45b6a
Andrey <dronisey@mail.ru>
2025-05-24 11:50:49 +0200

    3.13_korobko

---
+++ 3.13_korobko/3.13.1_korobko.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+enum Grade: String {
+case junior
+case middle
+case senior
+}
+
+enum Developer {
+    case IOS (grade: Grade)
+    case android (grade: Grade)
+    
+    
+    func description() -> String {
+        switch self {
+        case.IOS(let grade):
+            return "🍏 \(grade.rawValue) iOS-developer"
+        case .android(let grade):
+            return "🤖 \(grade.rawValue) Android-developer"
+        }
+    }
+}
+
+let developers: [Developer] = [.IOS(grade: .junior), .android(grade: .senior)]
+
+for developer in developers {
+    print(developer.description())
+}
