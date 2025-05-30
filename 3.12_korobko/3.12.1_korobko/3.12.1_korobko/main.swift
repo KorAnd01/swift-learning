779982dc988eb56a7a5c2787b22a12b6a42de103
Andrey <dronisey@mail.ru>
2025-05-30 15:32:25 +0200

    3.12_korobko

--- 3.12_korobko/3.12.1_korobko/3.12.1_korobko/main.swift
+++ 3.12_korobko/3.12.1_korobko/3.12.1_korobko/main.swift
@@ -1,31 +1,38 @@
 func isValidLength(_ input: String) -> Int? {
-    if let length = Int(input), length >= 4 {
-        return length
-    } else {
-        return nil
+    if input.count >= 4 {
+        return input.count
     }
+    return nil
 }
+
 
 func generatePassword(ofLength length: Int) -> String {
     let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let digits = "0123456789"
     let symbols = "!@#$%&*()"
-
+    
     var passwordChars: [Character] = []
-
-    passwordChars.append(digits.randomElement()!)
-    passwordChars.append(symbols.randomElement()!)
-    passwordChars.append(letters.randomElement()!)
-
-    let allChars = letters + digits + symbols
-
-    while passwordChars.count < length {
-        passwordChars.append(allChars.randomElement()!)
+    
+    if let digit = digits.randomElement() {
+        passwordChars.append(digit)
     }
-
-    // Перемешаем символы
+    if let symbol = symbols.randomElement() {
+        passwordChars.append(symbol)
+    }
+    if let letter = letters.randomElement() {
+        passwordChars.append(letter)
+    }
+    
+    let allChars = letters + digits + symbols
+    
+    while passwordChars.count < length {
+        if let randomChar = allChars.randomElement() {
+            passwordChars.append(randomChar)
+        }
+    }
+    
     passwordChars.shuffle()
-
+    
     return String(passwordChars)
 }
 
@@ -44,3 +51,4 @@ while true {
         }
     }
 }
+
